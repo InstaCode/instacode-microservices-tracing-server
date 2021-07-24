7ebaa558fea343f98473b7e1a7336c30ea00ad7d
Jason Edstrom <jason@instacode.io>
2021-07-24 15:28:16 -0500

    initial commit

---
+++ src/test/java/io/instacode/SpringContextTest.java
@@ -0,0 +1,17 @@
+package io.instacode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.instacode.spring.cloud.zipkin.ZipkinApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ZipkinApplication.class)
+public class SpringContextTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
